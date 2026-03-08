0c2f10a26f42000ff2df367eeb358e48506934bc
Sharad Aghara <sharadaghara24@gmail.com>
2026-03-08 18:58:37 +0530

    Initialized the project

---
+++ src/main/java/com/shawty/ShawtyApplication.java
@@ -0,0 +1,13 @@
+package com.shawty.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShawtyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShawtyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shawty/demo/ShawtyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shawty.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShawtyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
