a6f6b8da664465b77c14655e54ddc3aff36df082
Sharad Aghara <sharadaghara24@gmail.com>
2026-03-08 19:01:41 +0530

    Created url controller following mvc pattern

---
+++ src/main/java/com/shawty/controller/UrlController.java
@@ -0,0 +1,39 @@
+package com.shawty.controller;
+
+import com.shawty.dto.CreateUrlRequest;
+import com.shawty.dto.CreateUrlResponse;
+import com.shawty.service.UrlService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1/urls")
+@RequiredArgsConstructor
+public class UrlController {
+
+    private final UrlService service;
+
+    @PostMapping
+    public CreateUrlResponse createUrl(
+            @Valid
+            @RequestBody
+            CreateUrlRequest request
+    ) {
+        return service.createShortUrl(request);
+    }
+
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<Void> redirect(@PathVariable String shortCode) {
+        String originalUrl = service.getOriginalUrl(shortCode);
+
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .location(URI.create(originalUrl))
+                .build();
+    }
+}
