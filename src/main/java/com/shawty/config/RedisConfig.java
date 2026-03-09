ec6e9b394593cfc1bb380e0c0e8917abd7bd0dad
Sharad Aghara <sharadaghara24@gmail.com>
2026-03-10 00:27:05 +0530

    added cache manager for redis

--- src/main/java/com/shawty/config/RedisConfig.java
+++ src/main/java/com/shawty/config/RedisConfig.java
@@ -1,7 +1,11 @@
 package com.shawty.config;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @Configuration
 /*
@@ -10,4 +14,9 @@ import org.springframework.context.annotation.Configuration;
  */
 @EnableCaching
 public class RedisConfig {
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        return RedisCacheManager.create(connectionFactory);
+    }
 }
