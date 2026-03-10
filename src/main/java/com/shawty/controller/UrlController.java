a8af3b8eff10926494daf4d5614dd2f7cdd0275c
Sharad Aghara <sharadaghara24@gmail.com>
2026-03-11 00:44:31 +0530

    removed unused import

--- src/main/java/com/shawty/controller/UrlController.java
+++ src/main/java/com/shawty/controller/UrlController.java
@@ -4,7 +4,6 @@ import com.shawty.dto.CreateUrlRequest;
 import com.shawty.dto.CreateUrlResponse;
 import com.shawty.service.UrlService;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
